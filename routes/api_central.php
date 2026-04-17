3bf9f90da7192fc57605de455dce105b7989545b
Jason921See <jasonseejiajun@gmail.com>
2026-04-17 14:54:13 +0800

    Update api for central without prefix central

--- routes/api_central.php
+++ routes/api_central.php
@@ -3,12 +3,23 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Central\AuthController;
 
-Route::prefix('central')->group(function () {
+// Route::prefix('central')->group(function () {
 
-    Route::post('/login', [AuthController::class, 'login']);
+//     Route::post('/login', [AuthController::class, 'login']);
 
-    Route::middleware('auth:sanctum')->group(function () {
-        Route::get('/me', [AuthController::class, 'me']);
-        Route::post('/logout', [AuthController::class, 'logout']);
+//     Route::middleware('auth:sanctum')->group(function () {
+//         Route::get('/me', [AuthController::class, 'me']);
+//         Route::post('/logout', [AuthController::class, 'logout']);
+//     });
+// });
+
+Route::domain(config('app.domain'))
+    ->group(function () {
+        // Route::get('/', fn() => view('central.welcome'));
+        Route::post('/login', [AuthController::class, 'login']);
+
+        Route::middleware('auth:sanctum')->group(function () {
+            Route::get('/me', [AuthController::class, 'me']);
+            Route::post('/logout', [AuthController::class, 'logout']);
+        });
     });
-});
